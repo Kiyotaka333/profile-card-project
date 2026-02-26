6d9ea489ad5d09cb51cfba5d505a6b528b6d8ca1
rajkumar <kiyotakaayanokoji7785@gmail.com>
2026-02-26 09:02:15 +0545

    h

--- src/components/card/CardList.jsx
+++ src/components/card/CardList.jsx
@@ -1,14 +1,42 @@
-import CardItem from './CardItem';
-import userAvatarImg from '../../assets/images/tony.jpg';
-import classes from './CardList.module.css';
+import CardItem from "./CardItem";
+import userAvatarImg from "../../assets/images/tony.jpg";
+import classes from "./CardList.module.css";
 
-export default function CardList(){
- return(
-    <div className={classe.listContainer}> 
-        <CardItem name="Raj"  avatarImg={userAvatarImg}/>
-        <CardItem name="Ram" avatarImg={userAvatarImg}/>
-         <CardItem name="Rak" avatarImg={userAvatarImg}/>
-          <CardItem name="Ron" avatarImg={userAvatarImg}/>
+export default function CardList() {
+  return (
+    <div className={classes.listContainer}>
+      <CardItem
+        name="Raj"
+        avatarImg={userAvatarImg}
+        email="raj@gmail.com"
+        lacation="ktm"
+        experience="3 yrs"
+        role="CEO"
+      />
+      <CardItem
+        name="Ron"
+        avatarImg={userAvatarImg}
+        email="ron@gmail.com"
+        lacation="china"
+        experience="2 yrs"
+        role="employee"
+      />
+      <CardItem
+        name="Ram"
+        avatarImg={userAvatarImg}
+        email="ram@gmail.com"
+        lacation="pokhara"
+        experience="5 yrs"
+        role="hero"
+      />
+      <CardItem
+        name="Rak"
+        avatarImg={userAvatarImg}
+        email="rak@gmail.com"
+        lacation="America"
+        experience="4 yrs"
+        role="manager"
+      />
     </div>
- );
+  );
 }
--- src/components/card/Carditem.jsx
+++ src/components/card/Carditem.jsx
@@ -1,38 +1,42 @@
-import Tonystank from "../../assets/images/tony.jpg";
-import classes from "./CardItem.module.css";
 
-export default function CardItem(props) {
+import Card from './Card';
+import classes from './CardItem.module.css';
+
+export default function CardItem({name, avatarImg, email, location, experience,role}) {
   return (
-    <div className={classes.card}>
+    <Card>
       <div className={classes.header}>
-        <img className={classes.avatar} 
-        src={props.avatarImg}
-         alt="profile avatar" />
+        <img
+          className={classes.avatar}
+          src={avatarImg}
+          alt="profile avatar"
+        />
 
         <div className={classes.userInfo}>
-          <h2 className={classes.name}>{props.name}</h2>
+          <h2 className={classes.name}>{name}</h2>
+                <h2 className={classes.role}>{role}</h2>
           <p className={classes.role}>Frontend developer</p>
         </div>
       </div>
 
       <div className={classes.details}>
         <div className={classes.detailItem}>
-          <span className={classes.label}>Email:</span>
+          <span className={classes.label}>{email}</span>
           <span>raj@gamil.com</span>
         </div>
 
         <div className={classes.detailItem}>
-          <span className={classes.label}>Location:</span>
+          <span className={classes.label}>{location}</span>
           <span>Kathmandu, Nepal</span>
         </div>
 
         <div className={classes.detailItem}>
-          <span className={classes.label}>Experience:</span>
+          <span className={classes.label}>{experience}</span>
           <span>3 Years</span>
         </div>
       </div>
 
       <button className={classes.button}>View More</button>
-    </div>
+    </Card>
   );
 }
